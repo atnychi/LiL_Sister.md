56c9dbcec74b154a169c58a93b1d8594b1fd5378
ATNYCHI <ksystemsandsecurities@proton.me>
2025-05-25 14:13:16 -0500

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,15 @@
+SEED_EQUATIONS = [
+    "x**2 + y = 42",
+    "sin(x) + cos(y) = 0",
+    "z**3 - x*y = 100"
+]
+CROWNS = ["Crown1", "Crown2", "Crown3"]
+AUTHORIZED_RUNTIME_IDS = {
+    "1410-426-4743": "Brendon Joseph Kelly",
+    "3110-777-8844": "Robert Preston",
+    "850-737-2887": "Korre Fuller",
+    "2711-209-1010": "Christopher Cervantez",
+    "1999-111-4001": "Aaron Clark"
+}
+CROWN_SEAL = "⟁ΞΩ∞†"
+COSRL_LICENSE = "COSRL-LP v2.1"
